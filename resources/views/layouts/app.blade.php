feec3ef08b12c2e85e75958706651b2e27ff49bb
dim5ptr <145912072+dim5ptr@users.noreply.github.com>
2024-04-25 08:41:45 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,10 +12,6 @@
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link href="{{ asset('custom/style.css') }}" rel="stylesheet" />
     <link href="{{ asset('custom/overide.css') }}" rel="stylesheet" />
-    {{-- ? Javascript ? --}}
-    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
-    <script src="{{ asset('custom/script.js') }}"></script>
-    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
 </head>
 
 <body>
